ab571709356d51be0baf44318650dd87d933ba20
modthier <modthier47387@live.com>
2022-05-04 21:21:00 +0200

    initial commit

--- app/Mail/ScoreResultMail.php
+++ app/Mail/ScoreResultMail.php
@@ -6,19 +6,20 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\ScoreResult;
 
 class ScoreResultMail extends Mailable
 {
     use Queueable, SerializesModels;
-
+    public $scoreResult;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(ScoreResult $scoreResult)
     {
-        //
+        $this->scoreResult = $scoreResult;
     }
 
     /**
